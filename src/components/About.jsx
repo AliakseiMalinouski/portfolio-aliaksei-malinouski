0defd0532f9de91400f02a60afec3514909d9904
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-05-08 20:54:11 +0200

    add info about stack

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { Title } from "./Title";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
 import { techStackThunk } from "../Redux/About/techStackThunk";
+import { Tech } from "./Tech";
+import { useLocation } from "react-router-dom";
 
 export const About = React.memo(() => {
 
@@ -11,17 +13,25 @@ export const About = React.memo(() => {
 
     let dispatch = useDispatch();
 
+    let location = useLocation();
+
     const stackOfTech = useSelector(state => state.stack.stack);
 
     useEffect(() => {
         if(!stackOfTech.length) dispatch(techStackThunk);
     }, [dispatch, stackOfTech]);
 
-    console.log(stackOfTech)
+    let techMemoizeed = useMemo(() => stackOfTech && stackOfTech?.map((elem, index) => <Tech key={elem.id} custom={index} tech={location.pathname === '/' ? "" : elem.tech} icon={elem.icon} location={location.pathname === '/' ? location.pathname : ""}/>), [stackOfTech, location])
 
     return (
         <div className="About">
             <Title tag='h2' text="about-me" content={t}/>
+            <div className="FlexGroup">
+                <h4>{t('my-tech-stack')}</h4>
+                <ul className="StackOfTech">
+                    {techMemoizeed}
+                </ul>
+            </div>
         </div>
     )
 })
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import { techVariant } from "../motion variants/variants";
 import { motion } from "framer-motion";
 
-export const Tech = React.memo(({custom, tech, icon}) => {
-    return (
-        <motion.li variants={techVariant} initial={'hidden'} animate={'visible'} custom={custom}>
-            {tech} <img src={icon} alt='Tech'/>
-        </motion.li>
-    )
+export const Tech = React.memo(({custom, tech, icon, location}) => {
+    if(location === '/') {
+        return (
+            <motion.li variants={techVariant} initial={'hidden'} whileInView={'visible'} viewport={{once: true}} custom={custom}>
+                {tech} <img src={icon} alt='Tech'/>
+            </motion.li>
+        )
+    }
+    else {
+        return (
+            <motion.li variants={techVariant} initial={'hidden'} animate={'visible'} custom={custom}>
+                {tech} <img src={icon} alt='Tech'/>
+            </motion.li>
+        )
+    }
 })
