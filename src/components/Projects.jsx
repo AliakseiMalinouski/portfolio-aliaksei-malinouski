79efab0c5718483594029d44bb7a1317cd99169b
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-05-08 20:58:06 +0200

    dev projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { Project } from "./Project";
+import { useTranslation } from "react-i18next";
+import { useEffect, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { projectsInfoThunk } from "../Redux/Projects/projectsInfoThunk";
 
 export const Projects = React.memo(() => {
+
+
+    let {t} = useTranslation();
+    let dispatch = useDispatch();
+
+    const projects = useSelector(state => state.projects.projects);
+
+    useEffect(() => {
+        if(!projects.length) dispatch(projectsInfoThunk);
+    }, [projects, dispatch]);
+
+    const projectsMemoizeed = useMemo(() => projects.map(({id, apis, deploy, full, github, short, image, packages, read, stack, title, type}) => <Project
+    key={id}
+    apis={apis}
+    deploy={deploy}
+    full={full}
+    github={github}
+    short={short}
+    image={image}
+    packages={packages}
+    read={read}
+    stack={stack}
+    title={title}
+    type={type}
+    content={t}
+    />), [projects, t]); 
+
     return (
         <div className="Home">
-            Projects page
+            <div className="Projects">
+                {projectsMemoizeed}
+            </div>
         </div>
     )
 })
