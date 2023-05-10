c7a6741aae76f90767c855a536f58bc64853ccf3
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-05-10 19:29:13 +0200

    update title comp

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,14 +5,14 @@ import { motion } from "framer-motion";
 export const Title = React.memo(({tag, text, content}) => {
     if(tag === 'h2') {
         return (
-            <motion.h2 className="Title" variants={largeTitle} initial={'hidden'} whileInView={'visible'} viewport={{once: true}}>
+            <motion.h2 className="Title" variants={largeTitle} initial={'hidden'} animate={'visible'} viewport={{once: true}}>
                 {content(`${text}`)}
             </motion.h2>
         )
     }
     else {
         return (
-            <motion.h3 className="ProjectDetailsTitle" variants={largeTitleProjectsDetails} initial={'hidden'} whileInView={'visible'} viewport={{once: true}}>
+            <motion.h3 className="ProjectDetailsTitle" variants={largeTitleProjectsDetails} initial={'hidden'} animate={'visible'} viewport={{once: true}}>
                 {content(`${text}`)}
             </motion.h3>
         )
