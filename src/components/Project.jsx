fc3edb7e9b56b4f61351b152d24ac3000a13c523
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-05-11 22:01:11 +0200

    completed adaptive 720

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { projectVariant } from "../motion variants/variants";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@mui/material";
 
 export const Project = React.memo(({id, apis, deploy, full, github, short, image, packages, read, stack, title, type, content}) => {
+
+    const [windowWidthState, setWindowWidthState] = useState(false);
 
     let navigate = useNavigate();
 
@@ -13,12 +15,19 @@ export const Project = React.memo(({id, apis, deploy, full, github, short, image
         navigate(uri);
     }
 
+    useEffect(() => {
+        let windowWidth1024 = window.matchMedia("(max-width: 1024px)");
+        if(windowWidth1024.matches) setWindowWidthState(true);
+    }, []);
+
     return (
         <motion.div className="Project" variants={projectVariant} initial={'hidden'} whileInView={'visible'} custom={id} viewport={{once: true, amount: 0.2}}>
             <img className="ProjectImage" src={image} alt="Project" onClick={goToDetailsProjectParent}/>
             <div className="ShortProjectInfo">
                 <h3>{content(`${title}`)}</h3>
-                <p>{content(`${full}`)}</p>
+                {
+                    windowWidthState ? <p>{content(`${short}`)}</p> : <p>{content(`${full}`)}</p>
+                }
                 <Button variant="outlined" onClick={goToDetailsProjectParent}>
                     {content(`${read}`)}
                 </Button>
