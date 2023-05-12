79daceae223c10b4542243b9c939957d5c12f3ad
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-05-12 16:18:22 +0200

    drawer added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,7 @@ import { updateCurrentLanguage } from "../Redux/Header/languagesSlice";
 import { linkSpanHeader, activeLinkSpanHeader } from "../motion variants/variants";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Snack } from "./Snack";
+import { Drawer } from "@mui/material";
 
 export const Header = React.memo(() => {
 
@@ -25,6 +26,8 @@ export const Header = React.memo(() => {
 
     const [snakeState, setSnackState] = useState(false);
     const [snackType, setSnackType] = useState("");
+    const [headerState, setHeaderState] = useState(false);
+    const [drawerState, setDrawerState] = useState(false);
     
     let location = useLocation();
 
@@ -37,6 +40,11 @@ export const Header = React.memo(() => {
     useEffect(() => {
         if(!languages.length) dispatch(languagesThunk)
     }, [languages, dispatch]);
+
+    useEffect(() => {
+        let windowWidth = window.matchMedia('(max-width: 720px)');
+        if(windowWidth.matches) setHeaderState(true);
+    }, []);
 
     const changeCurrentLanguageParent = useCallback((language) => {
         changeLanguage(language);
@@ -53,16 +61,74 @@ export const Header = React.memo(() => {
     }, [changeCurrentLanguageParent]);
 
 
-    const linksMemoizeed = useMemo(() => links.map(({id, link, key}) => <Link key={id} link={link} translateKey={key} setLanguage={t} variant={linkSpanHeader} activeLink={activeLink} activeVariant={activeLinkSpanHeader}/>), [links, t, activeLink]);
+    const linksMemoizeed = useMemo(() => links.map(({id, link, key}) => <Link key={id} link={link} translateKey={key} setLanguage={t} variant={linkSpanHeader} activeLink={activeLink} activeVariant={activeLinkSpanHeader} headerState={headerState}/>), [links, t, activeLink, headerState]);
     const translateSelectMemoizeed = useMemo(() => <LanguageSelect key={1} languages={languages} content={t} currentLanguage={currentLanguage}/>, [languages, t, currentLanguage])
 
     return (
-        <div className="HeaderContent">
+        <div className="HeaderContent" style={{justifyContent: headerState ? 'space-between' : ""}}>
             {translateSelectMemoizeed}
-            <div className="HeaderLinks">
-                {linksMemoizeed}
-            </div>
-            <h3 className="Logo" onClick={() => navigate('/')}>Aliaksei.dev</h3>
+            {
+                !headerState
+                ?
+                <>
+                <div className="HeaderLinks">
+                    {linksMemoizeed}
+                </div>
+                <h3 className="Logo" onClick={() => navigate('/')}>Aliaksei.dev</h3>
+                </>
+            :
+            <img src="https://i.ibb.co/Krrtb7x/menu.png" alt="Menu" className="MenuButtonHeader"
+            onClick={() => {
+                setDrawerState(true);
+            }}
+            />
+            }
+            {
+                <Drawer anchor="left" open={drawerState} onClose={() => setDrawerState(false)}>
+                    <div style={{
+                        width: window.screen.width / 2,
+                        padding: '20px'
+                        }} className="WrapperDrawerContent">
+                        <h3 className="Logo" onClick={() => navigate('/')}
+                        style={{
+                            marginBottom: '20px'
+                        }}
+                        >Aliaksei.dev</h3>
+                        <div className="HeaderLinks">
+                            {linksMemoizeed}
+                        </div>
+                        <div style={{
+                            marginTop: '30px'
+                        }}>
+                        <a href="https://www.linkedin.com/in/aliaksei-malinouski-a44778249/" target="_blank" rel="noreferrer" style={{marginRight: '12px'}}>
+                            <img src="https://i.ibb.co/JdPdMNy/linkedin-2.png" alt='Linkedin'
+                            style={{width: '30px', heigth: '30px'}}
+                            />
+                        </a>
+                        <a href="https://github.com/AliakseiMalinouski" target="_blank" rel="noreferrer" style={{marginRight: '12px'}}>
+                            <img src="https://i.ibb.co/MgRCGH0/icons8-github-50.png" alt="GitHub"
+                            style={{width: '30px', heigth: '30px'}}
+                            />
+                        </a>
+                        <a href="mailto:aleksymalinowski21@gmail.com" target="_blank" rel="noreferrer" style={{marginRight: '12px'}}>
+                            <img src="https://i.ibb.co/CvNh5M4/email-1.png" alt="GitHub"
+                            style={{width: '30px', heigth: '30px'}}
+                            />
+                        </a>
+                        </div>
+                        <div>
+                        <p>
+                            <img src="https://i.ibb.co/YttZ8Xh/telegram.png" alt="Telegram"/>
+                            <span>@aleksymalinowski</span>
+                        </p>
+                        <p>
+                            <img src="https://i.ibb.co/R26DFFR/instagram.png" alt="Telegram"/>
+                            <span>aleksymalinowski_</span>
+                        </p>
+                        </div>
+                    </div>
+                </Drawer>
+            }
             <Snack open={snakeState} currentLanguage={currentLanguage} handleClose={() => setSnackState(false)} content={t} snackType={snackType}/>
         </div>
     )
--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,10 +3,13 @@ import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 
 
-export const Link = React.memo(({link, translateKey, setLanguage, variant, activeLink, activeVariant, isTransform}) => {
+export const Link = React.memo(({link, translateKey, setLanguage, variant, activeLink, activeVariant, isTransform, headerState}) => {
     if(activeLink === link) {
         return (
             <motion.div
+            style={{
+                marginBottom: headerState ? '20px' : ""
+            }}
             whileHover={{
                 scale: isTransform ? null : 1.08,
                 transition: {
@@ -29,6 +32,9 @@ export const Link = React.memo(({link, translateKey, setLanguage, variant, activ
     else {
         return (
             <motion.div
+            style={{
+                marginBottom: headerState ? '20px' : ""
+            }}
             whileHover={{
                 scale: isTransform ? null : 1.08,
                 transition: {
