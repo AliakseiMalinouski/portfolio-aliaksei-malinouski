6ceee35a636cfaf106cb390c5a20f996f7cf19a8
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-05-12 17:12:14 +0200

    adaptive

--- src/components/PersonalLink.jsx
+++ src/components/PersonalLink.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 export const PersonalLink = React.memo(({image, href, alt, styles, display, margin}) => {
     return (
-        <p style={display}>
-            <a href={href}>
-                <img src={image} alt={alt} style={styles}/>
-            </a>
+        <a href={href} style={display}>
+            <img src={image} alt={alt} style={styles}/>
             <span style={{marginLeft: margin}}>{alt}</span>
-        </p>
+        </a>
     )
 })
