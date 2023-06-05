ea25e1e908b284ad13cb4f1c858e3e5b3c4f5473
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-06-05 18:51:56 +0200

    update api link

--- src/Redux/Projects/projectsInfoThunk.js
+++ src/Redux/Projects/projectsInfoThunk.js
@@ -2,7 +2,7 @@ import { setProjectsInfo } from "./projectsInfoSlice";
 
 export const projectsInfoThunk = async (dispatch) => {
     try {
-        const response = await fetch('https://gist.githubusercontent.com/AliakseiMalinouski/485503c44c1039e01e0450aef957babb/raw/7a0f8709d186c1edcf3103b948ff29690f02c4f1/PortfolioProjectsInfo');
+        const response = await fetch('https://gist.githubusercontent.com/AliakseiMalinouski/485503c44c1039e01e0450aef957babb/raw/8b40020bd3fbace0d43a679ae83fe99b54129531/PortfolioProjectsInfo');
         if(response.ok) {
             const data = await response.json();
             dispatch(setProjectsInfo(data));
